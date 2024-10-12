50b691e83059ace92845d997b4ae66e7873db2e7
Ji ZHANG <hope.zh@gmail.com>
2024-10-12 22:33:36 +0800

    add nvim_treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,47 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+
+    build = ":TSUpdate",
+
+    -- This part is needed to enable highlighting for treesitter
+    config = function()
+        ---@diagnostic disable-next-line: missing-fields
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "css",
+                "git_config",
+                "gitcommit",
+                "gitignore",
+                "glsl",
+                "html",
+                "kdl",
+                "json",
+                "java",
+                "javascript",
+                "julia",
+                "latex",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "r",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end,
+}
