b9a9f3cf78ed38df6683512aeed50255e640b0c0
hopezh <hope.zh@gmail.com>
2024-10-12 22:00:00 +0800

    add render_markdown, quarto

---
+++ lua/plugins/render_markdown.lua
@@ -0,0 +1,19 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  opts = {
+    file_types = { "markdown", "quarto" },
+    code = { above = "", below = "" },
+  },
+
+  -- config = function()
+  --     require("render-markdown").setup({
+  --         file_types = { "markdown", "quarto" },
+  --     })
+  -- end,
+}
