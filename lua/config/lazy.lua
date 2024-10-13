83a4b12c066755bdfae3f8dae3da3ec95b813018
Ji ZHANG <hope.zh@gmail.com>
2024-10-13 11:35:05 +0800

    add python extras back

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.coding.mini-surround" },
         { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
+        { import = "lazyvim.plugins.extras.lang.python" },
 
         -- import/override with your plugins
         { import = "plugins" },
