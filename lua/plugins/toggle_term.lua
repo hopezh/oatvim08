76629d884d6dece851495c24950516e0748291ae
Ji ZHANG <hope.zh@gmail.com>
2025-02-21 13:46:48 +0800

    toggleterm width = 0.4*screen

--- lua/plugins/toggle_term.lua
+++ lua/plugins/toggle_term.lua
@@ -23,7 +23,7 @@ return {
             if term.direction == "horizontal" then
                 return 20
             elseif term.direction == "vertical" then
-                return vim.o.columns * 0.3
+                return vim.o.columns * 0.4
                 -- return 80
             end
         end,
