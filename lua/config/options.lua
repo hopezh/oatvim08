c46d5851422d23bd9598cac97f28c9208ab3bdd1
hopezh <hope.zh@gmail.com>
2025-02-25 14:00:56 +0800

    disable option setting for powershell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -43,17 +43,17 @@ vim.scriptencoding = "utf-8"
 -- Set to false to disable.
 vim.g.lazygit_config = false
 
--- make toggleterm work with powershell ----------------------------------------
--- ref: https://github.com/akinsho/toggleterm.nvim/wiki/Tips-and-Tricks#using-toggleterm-with-powershell
-local powershell_options = {
-    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-}
-
-for option, value in pairs(powershell_options) do
-    vim.opt[option] = value
-end
+-- -- [!!! conflict with Mac OS] make toggleterm work with powershell ----------------------------------------
+-- -- ref: https://github.com/akinsho/toggleterm.nvim/wiki/Tips-and-Tricks#using-toggleterm-with-powershell
+-- local powershell_options = {
+--     shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+--     shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+--     shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+--     shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+--     shellquote = "",
+--     shellxquote = "",
+-- }
+--
+-- for option, value in pairs(powershell_options) do
+--     vim.opt[option] = value
+-- end
