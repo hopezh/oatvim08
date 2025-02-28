3a9b02df1407ef9ec2aed6be456ee77787098f5e
Ji ZHANG <hope.zh@gmail.com>
2025-02-28 13:50:06 +0800

    deactivate indent color and tokyonight transparancy

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -7,37 +7,37 @@ return {
     ---@module "ibl"
     ---@type ibl.config
 
-    -- [since v3] use bg color for indentation area
-    config = function()
-        -- local highlight = {
-        --     "CursorColumn",
-        --     "Whitespace",
-        -- }
-
-        local highlight = {
-            "oat_grey",
-            "Whitespace",
-        }
-
-        local hooks = require("ibl.hooks")
-        -- create the highlight groups in the highlight setup hook, so they are reset
-        -- every time the colorscheme changes
-        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-            vim.api.nvim_set_hl(0, "oat_grey", { bg = "#252525" })
-        end)
-
-        require("ibl").setup({
-            indent = {
-                highlight = highlight,
-                char = "",
-            },
-            whitespace = {
-                highlight = highlight,
-                remove_blankline_trail = false,
-            },
-            scope = {
-                enabled = true,
-            },
-        })
-    end,
+    -- -- [since v3] use bg color for indentation area
+    -- config = function()
+    --     -- local highlight = {
+    --     --     "CursorColumn",
+    --     --     "Whitespace",
+    --     -- }
+    --
+    --     local highlight = {
+    --         "oat_grey",
+    --         "Whitespace",
+    --     }
+    --
+    --     -- create the highlight groups in the highlight setup hook, so they are reset
+    --     -- every time the colorscheme changes
+    --     local hooks = require("ibl.hooks")
+    --     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    --         vim.api.nvim_set_hl(0, "oat_grey", { bg = "#808080" })
+    --     end)
+    --
+    --     require("ibl").setup({
+    --         indent = {
+    --             highlight = highlight,
+    --             char = "",
+    --         },
+    --         whitespace = {
+    --             highlight = highlight,
+    --             remove_blankline_trail = false,
+    --         },
+    --         scope = {
+    --             enabled = true,
+    --         },
+    --     })
+    -- end,
 }
