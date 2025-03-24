3417ddbc6c5b860c2edcfc2fd7f954390ff72937
Ji ZHANG <hope.zh@gmail.com>
2025-03-24 16:13:21 +0800

    show todo in current buffer only

--- lua/plugins/todo_comments.lua
+++ lua/plugins/todo_comments.lua
@@ -1,33 +1,36 @@
 return {
     "folke/todo-comments.nvim",
 
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "ibhagwan/fzf-lua",
+    },
 
     version = "*", -- ensure using the latest version
 
     optional = true,
 
     keys = {
-        -- if using Telescope
-        {
-            "<leader>xo",
-            "<cmd>TodoLocList toggle keywords=oo<cr>",
-            desc = "show oat cmt in QuickFix",
-        },
-        {
-            "<leader>zor",
-            -- "<cmd>Trouble todo toggle<cr>",
-            "<cmd>Trouble todo toggle keywords=oo<cr>",
-            desc = "show oat cmt in Trouble",
-        },
-        {
-            "<leader>zot",
-            -- "<cmd>TodoTelescope<cr>",
-            "<cmd>TodoTelescope keywords=oo<cr>",
-            -- "<cmd>TodoTelescope keywords=oo cwd=false<cr>",
-            -- "<cmd>TodoTelescope keywords=oo cwd=%:p:h<cr>",
-            desc = "show oat cmt in Telescope",
-        },
+        -- -- if using Telescope
+        -- {
+        --     "<leader>xo",
+        --     "<cmd>TodoLocList toggle keywords=oo<cr>",
+        --     desc = "show oat cmt in QuickFix",
+        -- },
+        -- {
+        --     "<leader>zor",
+        --     -- "<cmd>Trouble todo toggle<cr>",
+        --     "<cmd>Trouble todo toggle keywords=oo<cr>",
+        --     desc = "show oat cmt in Trouble",
+        -- },
+        -- {
+        --     "<leader>zot",
+        --     -- "<cmd>TodoTelescope<cr>",
+        --     "<cmd>TodoTelescope keywords=oo<cr>",
+        --     -- "<cmd>TodoTelescope keywords=oo cwd=false<cr>",
+        --     -- "<cmd>TodoTelescope keywords=oo cwd=%:p:h<cr>",
+        --     desc = "show oat cmt in Telescope",
+        -- },
 
         -- -- if using fzf-lua from lazyvim v14 that replaces Telescope
         -- {
@@ -46,7 +49,42 @@ return {
         merge_keywords = true,
         keywords = {
             oo = { icon = "", color = "#505050" }, -- use '# oo:' to activate the keyword
-            -- oo: this is a comment marked by "oo"
         },
     },
+
+    -- === show custom todo "oo" in current buffer only ===
+
+    -- -- option 1: if using telescope instead of fzf
+    -- config = function(_, opts)
+    --     require("todo-comments").setup(opts)
+    --     vim.keymap.set("n", "<leader>st", function()
+    --         require("telescope.builtin").grep_string({
+    --             search = "oo|TODO|HACK|WARN|PERF|NOTE|FIX",
+    --             file_ignore_patterns = {},
+    --             only_sort_text = true,
+    --             path_display = { "smart" },
+    --             search_dirs = { vim.fn.expand("%:p") },
+    --         })
+    --     end, { desc = "Todo (current file)" })
+    -- end,
+
+    -- option 2: If using fzf-lua instead of telescope
+    -- stylua: ignore
+    config = function(_, opts)
+        require("todo-comments").setup(opts)
+        vim.keymap.set(
+            "n",
+            "<leader>zo",
+            function()
+                local dir = vim.fn.expand("%:p:h")
+                require("todo-comments.fzf").todo(
+                    {
+                        cwd = dir,
+                        query = vim.fn.expand("%:t"),
+                    }
+                )
+            end,
+            { desc = "Todo (current file)" }
+        )
+    end,
 }
