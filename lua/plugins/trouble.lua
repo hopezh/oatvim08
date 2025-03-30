b8049bc362dd9dc58d5ff096e61b5e62d136e049
hopezh <hope.zh@gmail.com>
2025-03-30 12:45:52 +0800

    show trouble in float window

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,18 @@
 return {
     "folke/trouble.nvim",
 
+    -- show trouble in a float window
+    opts = {
+        focus = true,
+        win = { type = "float" },
+        keys = {
+            -- The defaults for these are reversed but when having a floating window I want it to close on <cr>
+            o = "jump",
+            ["<cr>"] = "jump_close",
+        },
+    },
+    -- source: https://github.com/folke/trouble.nvim/discussions/333#discussioncomment-12619765
+
     -- opts = {
     --     mode = "document_diagnostics", -- Change the default mode
     -- },
