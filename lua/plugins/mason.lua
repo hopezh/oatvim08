0aadf7b19f598e859f8a8c6c180dff3f9b9cf3f7
hopezh <hope.zh@gmail.com>
2025-05-07 22:44:40 +0800

    mason major update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,34 +1,34 @@
 return {
-  "williamboman/mason.nvim",
+    "williamboman/mason.nvim",
 
-  opts = {
-    ensure_installed = {
-      "black",
-      "css-lsp",
-      "eslint-lsp",
-      "html-lsp",
-      "json-lsp",
-      "lua-language-server",
-      "markdownlint",
-      "marksman",
-      "mypy",
-      "pyright",
-      "shfmt",
-      "stylua",
-      "shellcheck",
-      "typescript-language-server",
-      "vim-language-server",
-      "yaml-language-server",
-    },
+    opts = {
+        ensure_installed = {
+            "black",
+            "css-lsp",
+            "eslint-lsp",
+            "html-lsp",
+            "json-lsp",
+            "lua-language-server",
+            "markdownlint",
+            "marksman",
+            "mypy",
+            "pyright",
+            "shfmt",
+            "stylua",
+            "shellcheck",
+            "typescript-language-server",
+            "vim-language-server",
+            "yaml-language-server",
+        },
 
-    ui = {
-      icons = {
-        -- package_installed = "✓",
-        -- package_pending = "➜",
-        package_installed = "",
-        package_pending = "➜",
-        package_uninstalled = "",
-      },
+        ui = {
+            icons = {
+                -- package_installed = "✓",
+                -- package_pending = "➜",
+                package_installed = "",
+                package_pending = "➜",
+                package_uninstalled = "",
+            },
+        },
     },
-  },
 }
