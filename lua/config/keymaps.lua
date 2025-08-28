fd8fe6382e2a6fa4093df32e1b81ea9781bbb7c7
Ji ZHANG <hope.zh@gmail.com>
2025-08-28 22:50:32 +0800

    add keymap to insert python code blk in quarto qmd file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,6 +10,27 @@ vim.keymap.set("s", "jk", "<ESC>") -- select mode
 vim.keymap.set("v", "jk", "<ESC>") -- visual & select mode
 vim.keymap.set("x", "jk", "<ESC>") -- visual mode
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "quarto",
+    callback = function()
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>q"] = {
+                p = {
+                    function()
+                        local lines = { "```{python}", "", "```" }
+                        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+                        vim.api.nvim_buf_set_lines(0, row, row, false, lines)
+                        vim.api.nvim_win_set_cursor(0, { row + 2, 0 })
+                        vim.cmd("startinsert")
+                    end,
+                    "Insert Python code block",
+                },
+            },
+        }, { buffer = true })
+    end,
+})
+
 -- switch btw terimal and editor when using toggleterm -------------------------
 -- function _G.set_terminal_keymaps()
 --     local opts = { buffer = 0 }
